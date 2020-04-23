756a2704a4d3a6317458365eb43854fc5929cef1
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-23 18:27:32 +0530

    Modified Code as Per Suggestions Given In Review

--- Quantity Measurement/app/QuantityMeasurementMain.js
+++ Quantity Measurement/app/QuantityMeasurementMain.js
@@ -6,7 +6,7 @@ log("Quantity Measurement Using Node JS");
 
 class QuantityMeasurementMain
 {
-
+    // Method For Check Length and Units are Same
     checkLengthOfUnits(unit1, unit2, value1, value2)
     {
         if (unit1 == unit2 && value1 == value2)
@@ -14,30 +14,37 @@ class QuantityMeasurementMain
         else
             return false
     }
-    compare(input, value){
+    // Method To Compare Value and Units and Returns Conversion
+    compare(input, value)
+    {
         let unit=input.toLowerCase()
+
         if (unit == 'feet')
-            return value*Units.FEET
+            return value*Units.FEET //return value*12
         if (unit == 'yard')
-            return value*Units.YARD
+            return value*Units.YARD //return value*36
         if (unit == 'cm')
-            return value*Units.CM
+            return value*Units.CM //return value*0.4
 
         if (unit == 'gallon')
-            return value*Units.GALLON
+            return value*Units.GALLON //return value*3.78
         if (unit == 'ml')
-            return value*Units.ML
+            return value*Units.ML //return value*0.001
 
         if (unit == 'gm')
-            return value*Units.GM
+            return value*Units.GM //return value*0.0001
         if (unit == 'ton')
-            return value*Units.TON
+            return value*Units.TON //return value*1000
+
         if (unit == 'farenheit')
-            return ((value - 32) * 5/9)
+            return ((value - 32) * 5/9) //return converted from Farenheit To Celcius
         else
-            return value
+            return value //return Same value Of Units => Inch, Liter, Kilogram & Celcius
     };
-    addition(value1, value2){
+
+    // Method For Adding Values of Units
+    addition(value1, value2)
+    {
         return value1+value2;
     }
 }
--- Quantity Measurement/app/Units.js
+++ Quantity Measurement/app/Units.js
@@ -1,13 +1,14 @@
-const UNITS={
-    FEET:12,
-    INCH:1,
-    YARD:36,
-    CM:0.4,
-    ML:0.001,
-    LT:1,
-    GALLON:3.78,
-    KG:1,
-    GM:0.001,
-    TON:1000
-};
+const UNITS=
+    {
+        FEET:12,
+        INCH:1,
+        YARD:36,
+        CM:0.4,
+        ML:0.001,
+        LT:1,
+        GALLON:3.78,
+        KG:1,
+        GM:0.001,
+        TON:1000
+    };
 module.exports=UNITS
