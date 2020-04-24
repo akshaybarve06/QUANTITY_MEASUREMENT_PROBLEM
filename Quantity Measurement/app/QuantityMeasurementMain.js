144f9dd092757dc4ba092df9132a71d295038ea1
akshaybarve06 <akshaybarve06@gmail.com>
2020-04-24 12:36:40 +0530

    Refactored: Removed Empty Lines, Done Proper Indentation and Done Commenting

--- Quantity Measurement/app/QuantityMeasurementMain.js
+++ Quantity Measurement/app/QuantityMeasurementMain.js
@@ -1,5 +1,4 @@
 // Quantity Measurement Using Node-JS
-
 let log=console.log;
 let Units= require('../app/Units');
 log("Quantity Measurement Using Node JS");
@@ -18,30 +17,25 @@ class QuantityMeasurementMain
     compare(input, value)
     {
         let unit=input.toLowerCase()
-
         if (unit == 'feet')
             return value*Units.FEET //return value*12
         if (unit == 'yard')
             return value*Units.YARD //return value*36
         if (unit == 'cm')
             return value*Units.CM //return value*0.4
-
         if (unit == 'gallon')
             return value*Units.GALLON //return value*3.78
         if (unit == 'ml')
             return value*Units.ML //return value*0.001
-
         if (unit == 'gm')
             return value*Units.GM //return value*0.0001
         if (unit == 'ton')
             return value*Units.TON //return value*1000
-
         if (unit == 'farenheit')
             return ((value - 32) * 5/9) //return converted from Farenheit To Celcius
         else
             return value //return Same value Of Units => Inch, Liter, Kilogram & Celcius
-    };
-
+    }
     // Method For Adding Values of Units
     addition(value1, value2)
     {
--- Quantity Measurement/app/Units.js
+++ Quantity Measurement/app/Units.js
@@ -1,6 +1,6 @@
 const UNITS=
     {
-    // Constant For Multiply with Given Units
+        // Constant For Multiply with Given Units
         FEET:12,
         INCH:1,
         YARD:36,
